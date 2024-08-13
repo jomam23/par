7f53c811500753cc176c6d732584adf6a8733e77
Hickory programes LTD <126360979+jomam23@users.noreply.github.com>
2024-08-12 17:51:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+// Add a new post
+function addPost() {
+    const postContent = document.getElementById('postContent').value;
+    if (postContent.trim() !== '') {
+        const postsContainer = document.getElementById('comments');
+        const post = document.createElement('div');
+        post.className = 'post';
+        post.textContent = postContent;
+        postsContainer.prepend(post);
+        document.getElementById('postContent').value = '';
+    }
+}
+function Home(){
+    location.href = '/Home.htm';
+}
+function aco(){
+    location.href = '/aco.html';
+}
